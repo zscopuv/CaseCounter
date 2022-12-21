d4331df4d08db2d448e954d2731c73433b5d86b3
zscopuv <87715150+zscopuv@users.noreply.github.com>
2022-12-21 07:15:54 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import cases
+import os
+os.system("cls")
+
+print("\n\n")
+
+# The actual code
+print(cases.display(1))
+cases.edit(1, "Hello, world!")
+print(cases.display(1))
+
+cases.delete(150)
+
+print("\n\n")
